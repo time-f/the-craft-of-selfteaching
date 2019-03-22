0a29ef91edc0cdf9c109ced98fc8a6b6a898f911
time-f <ZBS_IT@163.com>
2019-03-22 23:44:00 +0800

    my notes

---
+++ my-notes/my-notes.py
@@ -0,0 +1,6 @@
+# 文档生成工具 http://www.sphinx-doc.org/en/master/
+# python教程 https://docs.python.org/3/tutorial/index.html
+# python教程中文版 https://docs.python.org/zh-cn/3/tutorial/index.html(for v.3.7.2）
+# python教程中文版 http://www.pythondoc.com/pythontutorial3/ (for v.3.6.3)
+# 人人都能用英语 https://github.com/xiaolai/everyone-can-use-english.git
+
