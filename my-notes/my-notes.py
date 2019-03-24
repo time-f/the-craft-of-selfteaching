d684fc59b3150f04c11d297a65ad4cbb177f5d4c
time-f <ZBS_IT@163.com>
2019-03-24 21:43:45 +0800

    3c

--- my-notes/my-notes.py
+++ my-notes/my-notes.py
@@ -1,6 +1,20 @@
 # 文档生成工具 http://www.sphinx-doc.org/en/master/
+# python源码 https://github.com/python/
+# python demo程序: https://github.com/python/cpython/tree/master/Tools/demo
 # python教程 https://docs.python.org/3/tutorial/index.html
+# python标准库 https://docs.python.org/3/library/index.html
 # python教程中文版 https://docs.python.org/zh-cn/3/tutorial/index.html(for v.3.7.2）
 # python教程中文版 http://www.pythondoc.com/pythontutorial3/ (for v.3.6.3)
 # 人人都能用英语 https://github.com/xiaolai/everyone-can-use-english.git
-
+# 中国人写的英文书 张纯如的书很值得一看；郑念的 Life and Death in Shanghai 费正清的剑桥中国史（The Cambridge History of China）
+# 民国时代的作者连阔如先生写的《江湖丛谈》
+# Python 中有一个概念叫 PEP，Python Enhancement Proposals https://www.python.org/dev/peps/pep-0008/
+# python测试模块: https://docs.python.org/3/library/unittest.html
+# 开发方法论 https://en.wikipedia.org/wiki/MoSCoW_method
+# 开发方法论  MoSCoW Method 两个 o 字母放在那里，是为了能够把这个缩写读出来，发音跟莫斯科一样
+            '''
+            Must have
+            Should have
+            Could have
+            Won't have
+            '''
